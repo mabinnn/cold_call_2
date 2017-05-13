c6a6b649dddd26a2e19d77b34ac7c0e0cb084b2d
Marvin Alganes <marvin.alganes@gmail.com>
2017-05-12 23:23:11 -0700

    initial commit

--- cold_call_2/ViewController.swift
+++ cold_call_2/ViewController.swift
@@ -9,15 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let cohortNames = ["Marvin", "Kevin", "Masato", "Kavya", "Shruta"]
+    
+    
+    @IBAction func coldButton(_ sender: UIButton) {
+        let idx = Int(arc4random_uniform(UInt32(cohortNames.count)))
+        print("Button is pressed at tag \(sender.tag)")
+        
+        // the condition should refer to index, but printing, it should add value of 1 so it excludes 0.
+        
+        if idx == 0  || idx == 1{
+            numberLabel.textColor = UIColor.red
+        }
+        if idx == 2  || idx == 3{
+            numberLabel.textColor = UIColor.orange
+        }
+        if idx == 4 {
+            numberLabel.textColor = UIColor.green
+        }
+        
+        
+        nameLabel.text = cohortNames[idx]
+        numberLabel.text = String(idx + 1)
+    }
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        nameLabel.text = "Ready?"
+        numberLabel.text = ""
     }
 
 
